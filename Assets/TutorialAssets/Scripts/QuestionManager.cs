8981d14b3846830862a8665a5ba39f86136107e2
Yashna Islam <yashna.islam.052@gmail.com>
2022-01-10 23:40:15 -0700

    Week2: methods and parameters
    added answer validation using methods and parameters using out modifier

--- Assets/TutorialAssets/Scripts/QuestionManager.cs
+++ Assets/TutorialAssets/Scripts/QuestionManager.cs
@@ -7,31 +7,58 @@ public class QuestionManager : MonoBehaviour
 {
    [SerializeField] private TMP_Text messageBoxTextField;
     [SerializeField] TMP_InputField answerInputField;
-
-    private string question;
     [SerializeField] private int answer;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        GenerateQuestions();
+    }
+
+    void GenerateQuestions()
+    {
+       AddSubtractQuestion(100, out string question, out answer);
+
+        messageBoxTextField.text = question;
+        ClearInputField();
+    }
+
+    void AddSubtractQuestion(int maxRange, out string question, out int qA)
+    {
         //Find Random numbers for operand
-        int operand1 = Random.Range(1, 100);
-        int operand2 = Random.Range(1, 100);
-       
-        if(Random.value < 0.5f) // Returns a random float within [0.0 to 1.0] range is inclusive.
+        int operand1 = Random.Range(1, maxRange);
+        int operand2 = Random.Range(1, maxRange);
+
+        if (Random.value < 0.5f) // Returns a random float within [0.0 to 1.0] range is inclusive.
         {
             question = $"{operand1} + {operand2} = ? ";
-            answer = operand1 + operand2;
+            qA = operand1 + operand2;
         }
         else
         {
-             question= $"{operand1} - {operand2} = ?";
-             answer = operand1 - operand2;
+            question = $"{operand1} - {operand2} = ?";
+            qA = operand1 - operand2;
         }
 
-        messageBoxTextField.text = question;
-        answerInputField.Select();
+    }
 
+    public void ValidateAnswer()
+    {
+        if (answerInputField.text == answer.ToString())
+        {
+            GenerateQuestions();
+        }
+        else
+        {
+            ClearInputField();
+        }
+    }
+
+    void ClearInputField()
+    {
+        answerInputField.text = "";
+        answerInputField.ActivateInputField();
     }
 
     // Update is called once per frame
